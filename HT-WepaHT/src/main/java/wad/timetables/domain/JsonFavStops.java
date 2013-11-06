43975bad775998330ffcecf8fc1b9b5af741fac2
Marko Haanranta <marko.haanranta@gmail.com>
2013-11-06 17:25:20 +0200

    json started

---
+++ HT-WepaHT/src/main/java/wad/timetables/domain/JsonFavStops.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.timetables.domain;
+
+/**
+ *
+ * @author Marko
+ */
+public class JsonFavStops {
+    
+    private String searchName;
+    
+    private String stopNumber;
+    
+    private String stopName;
+
+    public String getSearchName() {
+        return searchName;
+    }
+
+    public void setSearchName(String searchName) {
+        this.searchName = searchName;
+    }
+
+    public String getStopNumber() {
+        return stopNumber;
+    }
+
+    public void setStopNumber(String stopNumber) {
+        this.stopNumber = stopNumber;
+    }
+
+    public String getStopName() {
+        return stopName;
+    }
+
+    public void setStopName(String stopName) {
+        this.stopName = stopName;
+    }
+    
+    
+}
--- HT-WepaHT/src/main/java/wad/timetables/service/SavedSearchService.java
+++ HT-WepaHT/src/main/java/wad/timetables/service/SavedSearchService.java
@@ -1,6 +1,7 @@
 package wad.timetables.service;
 
 import java.util.List;
+import wad.timetables.domain.JsonFavStops;
 import wad.timetables.domain.SavedSearch;
 import wad.timetables.domain.User;
 
@@ -10,4 +11,5 @@ public interface SavedSearchService {
     SavedSearch createSavedSearch(SavedSearch savedSearch);
     SavedSearch deleteSavedSearch(String searchName);
     List<SavedSearch> listSavedSearches(String username);
+    List<JsonFavStops> returnFavouriteStops(String username);
 }
